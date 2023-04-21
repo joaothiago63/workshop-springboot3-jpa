cfee45b032cc225a3bc3f28aaffc5c3df15674e4
João Thiago <contato_joaothiago@hotmail.com>
2023-04-21 17:23:48 -0300

    Project created

---
+++ src/test/java/com/joao_roberto/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao_roberto.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
