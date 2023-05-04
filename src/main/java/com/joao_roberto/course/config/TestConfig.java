07b0fd84e71926427af5a0caaa5cf2eced2f4acd
João Thiago <contato_joaothiago@hotmail.com>
2023-05-04 09:43:03 -0300

    Many-To-Many association with jointable

--- src/main/java/com/joao_roberto/course/config/TestConfig.java
+++ src/main/java/com/joao_roberto/course/config/TestConfig.java
@@ -46,6 +46,25 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category(null, "Electronics"); 
 		Category cat2 = new Category(null, "Books"); 
 		Category cat3 = new Category(null, "Computers"); 
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
+		
+
+
+
+		
+
+		
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
@@ -60,8 +79,7 @@ public class TestConfig implements CommandLineRunner {
 
 	    userRepository.saveAll(Arrays.asList(u1,u2,u3));
 	    orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4));
-	    categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-	   productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+	    
 	    
 	}
 	
